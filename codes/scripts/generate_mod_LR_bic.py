9aaacc4c00fa4d2b5ea3f3abbff7a4b3bae2ef37
LikeJust <diddydwns135@gmail.com>
2019-06-14 14:11:49 +0900

    resize & scale func

--- codes/scripts/generate_mod_LR_bic.py
+++ codes/scripts/generate_mod_LR_bic.py
@@ -13,19 +13,16 @@ def generate_mod_LR_bic():
     mod_scale = 4
 
     # set data dir
-    sourcedir = 'D:\\tmp\\BasicSR\\data_samples\\train_HR'
-    savedir = 'D:\\tmp\\BasicSR\\data_samples\\train_HR_mod'
+    sourcedir = 'D:\\tmp\\BasicSR\\data_samples\\div_train\\valid_HR'
+    savedir = 'D:\\tmp\\BasicSR\\data_samples\\div_train\\valid_LR'
 
     if not os.path.isdir(savedir):
         os.mkdir(savedir)
         
-    if not os.path.isdir(os.path.join(savedir, 'LR')):
-        os.mkdir(os.path.join(savedir, 'LR'))
-    
-    filepaths = [f for f in os.listdir(sourcedir) if f.endswith('.png')]
+    filepaths = [f for f in os.listdir(sourcedir) if f.endswith('.png')][:10]
     pbar = tqdm(total=len(filepaths))
 
-    def resize_func(filename):
+    def scale_func(filename):
         # read image
         image = cv2.imread(os.path.join(sourcedir, filename))
     
@@ -41,16 +38,24 @@ def generate_mod_LR_bic():
         cv2.imwrite(os.path.join(savedir, filename), image_LR)
 
         pbar.update(1)
+
+    def resize_func(filename):
+        # read image
+        image = cv2.imread(os.path.join(sourcedir, filename))
+        image_LR = cv2.resize(image, dsize=(128, 128), interpolation=cv2.INTER_AREA)
+        cv2.imwrite(os.path.join(savedir, filename), image_LR)
+
+        pbar.update(1)
     
     # prepare data with augementation
 
-    # # for iter
+    # for iter
     # for filename in filepaths:
     #     resize_func(filename)
     
     # ThreadPool
     with ThreadPoolExecutor() as executor:
-        executor.map(resize_func, filepaths)
+        executor.map(scale_func, filepaths)
 
 
 if __name__ == "__main__":
